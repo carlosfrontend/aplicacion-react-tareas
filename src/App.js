dde9326f307fe09ccbd5c13035adb674d17c6d2d
Carlos Pulido <carlosfrontend@hotmail.com>
2023-05-31 00:46:41 +0200

    Añadidos espacios

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const obtenerTareasAlmacenadas = () => {
 };
 
 function App() {
+
   const [ tareas,setTareas ] = useState(obtenerTareasAlmacenadas()); /* Actualiza el estado con las tareas almacenadas */
 
   /* Guarda las tareas en localStorage con el nombre lista */
@@ -35,30 +36,41 @@ function App() {
   const  agregarTarea = tarea => {
     
     if(tarea.texto.trim()){
+
       tarea.texto = tarea.texto.trim();
       const tareasActualizadas = [tarea, ...tareas]; /* ... operador spread */
       setTareas(tareasActualizadas);
+
     }
   };
 
   const eliminarTarea = id => {
+
     /* Crea un nuevo Array con todas las tareas menos la tarea en la que estamos pinchando  (parametro de nuestra  funcion ) */
     const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
     setTareas(tareasActualizadas);
+
   };
 
   const completarTarea = id => {
+
     const tareasActualizadas = tareas.map(tarea => {
+
       if(tarea.id === id){
+
         tarea.completada = !tarea.completada;
+
       }
+
       return tarea;
+
     });
 
     setTareas(tareasActualizadas);
   };
 
   return (
+    
     <div className="aplicacion-tareas">
       <LogoFreeCodeCamp />
       <div className='tareas-lista-principal'>
